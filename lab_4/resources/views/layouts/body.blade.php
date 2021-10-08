d7a8cd7df4df072f6f9ca93088d5d6962edf6707
MD WALI MOSNAD AYSHIK <61822224+Ayshik@users.noreply.github.com>
2021-10-08 17:05:50 +0600

    lab 4

---
+++ lab_4/resources/views/layouts/body.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="CSS/login.css">
+
+    </head>
+    <body>
+       
+   
+
+        <div>
+            @yield('content')
+        </div>
+ 
+    </body>
+</html>
---
+++ lab_4/resources/views/login.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.body')
+@section('content')
+
+<div class="login">
+    <input type="text" placeholder="Phone" id="phone">  
+  <input type="password" placeholder="password" id="password">  
+  <a href="#" class="forgot">forgot password?</a>
+  <input type="submit" value="Sign In">
+</div>
+<div class="shadow"></div>
+
+   
+        
+       @endsection
+       
+   
